76eabb6251d2f696736cc03194f0f91f5d9d6fdb
Juri <papkoi@mail.ru>
2020-12-04 21:25:49 +0300

    +Умножение переменных

--- PHP/index.php
+++ PHP/index.php
@@ -6,6 +6,12 @@
     <title>Document</title>
 </head>
 <body>
-    <?php echo 'Hello World' ?>
+    <?php 
+    $a=100; 
+    $b=1000;
+    echo $a*$b.'<br>';
+    echo 5**2;
+    ?>
+    
 </body>
 </html>
