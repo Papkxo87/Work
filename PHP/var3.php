18cfc9c8a6e054ffa9d5124014434726af1db8ce
Juri <papkoi@mail.ru>
2020-12-08 21:19:32 +0300

    +уравнение

--- PHP/var3.php
+++ PHP/var3.php
@@ -9,20 +9,20 @@
 
 <body>
     <?php
-    /*$a = 9;
-    $b = 3;
-    if ($b > $a) {
-        echo 'yes';
-        echo '!';
-    } else {
-        echo 'no';
-        echo '!';
-    }*/
+    /*$x = 25;
+    $y = sqrt($x) / ($x ** 2);
+    echo $y;*/
 
-    $x = 10;
-    $y = $x ** 2;
-    echo $y;
+    /*$a = 4;
+    $b = 8;
+    $y = abs($a - $b) / sin($b) * sqrt($b + $a);
+    echo $y;*/
 
+    $a = 4;
+    $b = 80;
+    $c = 2;
+    $x = (-$b + sqrt($b ** 2 - 4 * $a * $c)) / (2 * $a);
+    echo $x;
     ?>
 
 </body>
