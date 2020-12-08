3199c01d9f7359bec29866fe226addb59e9154fe
Juri <papkoi@mail.ru>
2020-12-08 20:00:47 +0300

    add

---
+++ PHP/index1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $a = 10;
+    $b = 100;
+    $c = 20;
+    $d = 9;
+    echo $a * $b . '<br>';
+    echo $a + $b + $c . '<br>';
+    echo $b / $a . '<br>';
+    echo $b - $a . '<br>';
+    echo $c % $d . '<br>';
+    ?>
+
+</body>
+
+</html>
