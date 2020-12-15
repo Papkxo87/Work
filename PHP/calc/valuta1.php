eb8af5a6e94491b885408e1dee530d52db725311
Juri <papkoi@mail.ru>
2020-12-15 19:34:54 +0300

    ВУЗ

--- PHP/calc/valuta1.php
+++ PHP/calc/valuta1.php
@@ -1,8 +1,9 @@
 <?php
-$bel = $_POST["bel"];
-$dol=$bel*2.65;
-$euro=$bel*3.25;
-$rus=$bel*2.11;
-echo 'USD  '.$dol.'<br>';
-echo 'EUR  '.$euro.'<br>';
-echo 'RUB  '.$rus.'<br>';
+$rezult = $_POST["rezult"];
+$bal = 10;
+$rezult;
+if ($rezult >= $bal) {
+    echo "ВЫ ПОСТУПИЛИ В ВУЗ";
+} else {
+    echo "ВЫ НЕ ПОСТУПИЛИ В ВУЗ";
+}
