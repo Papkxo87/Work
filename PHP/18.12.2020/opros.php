3e47161d6351f56697069398ca0231f7d6e070ec
Juri <papkoi@mail.ru>
2020-12-18 21:12:14 +0300

    опросник для ДЗ

---
+++ PHP/18.12.2020/opros.php
@@ -0,0 +1,10 @@
+<?php
+$op1 = $_POST["op1"];
+$op2 = $_POST["op2"];
+$op3 = $_POST["op3"];
+
+if($op1==4 && $op2=='Tokio' && $op3==28 ){
+    echo 'Wellcome to final';
+}else{
+    echo 'Ohh noooo....';
+}
