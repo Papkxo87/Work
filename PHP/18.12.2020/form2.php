78f0e94d7e47071121a6aa8112f6f1850fb67286
Juri <papkoi@mail.ru>
2020-12-18 20:18:34 +0300

    строки

--- PHP/18.12.2020/form2.php
+++ PHP/18.12.2020/form2.php
@@ -7,8 +7,31 @@
 }*/
 
 //add YES
-if(true or !true ){
+/*if(true or !true ){
     echo "YES";
 }else{
+    echo "NO";
+}*/
+
+/*if (0 xor 0) {
+    echo "YES";
+} else {
+    echo "NO";
+}*/
+
+//выводит число а потом прибавляет и выводит
+/*$a=2;
+echo $a--;
+echo "<br>";
+echo $a;*/
+
+//определяет тип переменной
+/*$a=12.1;
+echo gettype($a);*/
+
+$a = 12;
+if (is_int($a)) {
+    echo "YES";
+} else {
     echo "NO";
 }
