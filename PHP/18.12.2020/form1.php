d9bc2b903995140b2209f7bc1364fa051d8c3019
Juri <papkoi@mail.ru>
2020-12-18 19:05:43 +0300

    add or and

--- PHP/18.12.2020/form1.php
+++ PHP/18.12.2020/form1.php
@@ -1,8 +1,36 @@
 <?php
-$a = 5;
-$b = 3;
+//$a = 2;
+//$b = 3;
 
-if(!($a>$b)){
+/*if(!($a>$b)){
+    echo "YES";
+}else{
+    echo "NO";
+}*/
+
+//echo ($a > $b)*1;
+
+/*if(true===1){
+    echo "YES";
+}else{
+    echo "NO";
+}*/
+
+/*if(true==1){
+    echo "YES";
+}else{
+    echo "NO";
+}*/
+
+//add NO
+/*if(true and !true ){
+    echo "YES";
+}else{
+    echo "NO";
+}*/
+
+//add YES
+if(true or !true ){
     echo "YES";
 }else{
     echo "NO";
---
+++ PHP/18.12.2020/form2.php
@@ -0,0 +1,14 @@
+<?php
+//add NO
+/*if(true and !true ){
+    echo "YES";
+}else{
+    echo "NO";
+}*/
+
+//add YES
+if(true or !true ){
+    echo "YES";
+}else{
+    echo "NO";
+}
