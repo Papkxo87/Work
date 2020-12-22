9ed08a40bc73cb3bd6ff922cb5ee8ed3fdb90f54
Juri <papkoi@mail.ru>
2020-12-22 20:20:57 +0300

    добавить, удалить элемент

--- PHP/22.12.2020/arr.php
+++ PHP/22.12.2020/arr.php
@@ -1,6 +1,27 @@
 <?php
-$arr= array("somearray"=>array(6=>5,13=>9,"a"=>11));
+/*$arr= array("somearray"=>array(6=>5,13=>9,"a"=>11));
 
 echo $arr["somearray"] [6].'<br>';
 echo $arr["somearray"] [13].'<br>';
-echo $arr["somearray"] ["a"];
+echo $arr["somearray"] ["a"].'<br>';
+
+$arr1=array(5=>30,44,56,22,"a"=>1111);
+
+$arr2=array(5=>30,6=>44,7=>56,8=>22,"a"=>1111);
+echo "<pre>";
+print_r($arr1);
+echo $arr1.'<br>';
+print_r($arr2);
+
+echo $arr2;*/
+
+$arr1 = [5 => 30,6 => 44,7 => 56, 8 =>22, 9 =>1111];
+$arr1[] = 4444;//добавить 
+print_r($arr1);
+echo $arr1.'<br>';
+$arr1["z"] = 77777777;//добавить
+print_r($arr1);
+echo $arr1.'<br>';
+unset($arr1[7]);//удалить элемент
+print_r($arr1);
+echo $arr1;
